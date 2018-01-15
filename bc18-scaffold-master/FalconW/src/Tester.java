122cb5cc798266e4087c5db90df63d07863cc27a
kevin-f-liu <kevin.f.liu2012@gmail.com>
2018-01-15 00:57:25 -0500

    I think I have a working A* module. Needs testing

---
+++ bc18-scaffold-master/FalconW/src/Tester.java
@@ -0,0 +1,14 @@
+
+public class Tester {
+	private int x;
+	private int y;
+	private char content;
+	
+	public Tester(int x, int y, char content) {
+		
+	}
+	
+	public boolean isWalkable() {
+		
+	}
+}
