fdd09198ede44140b4fba75566aa9fb81694a082
prashantRR1st <prashxlr8@gmail.com>
2018-01-19 18:24:51 -0500

    Dynamic Research Management Class

---
+++ bc18-scaffold-master/FalconBot/ResearchManagement.java
@@ -0,0 +1,42 @@
+import bc.*;
+
+// Class that serves to make Macro Decisions on Player's Resource Management
+public class ResearchManagement {
+
+	private GameController gc;
+	
+	
+	public ResearchManagement(GameController gc){
+		this.gc = gc;
+
+	}
+	
+	public createDefaultQueue(GameController gc){
+
+	}
+
+	public updateQueueCheck(GameController gc){
+		
+	}
+	
+	public raceToMarsProtocol(GameController gc){
+		
+	}
+
+	public blinkRushProtocol(GameController gc){
+		
+	}
+	
+	public snipeRushProtocol(GameController gc){
+		
+	}
+	
+	public javelinRushQueue(GameController gc){
+		
+	}
+
+	public workerBoostProtocol(GameController gc){
+		
+	}
+	
+}
