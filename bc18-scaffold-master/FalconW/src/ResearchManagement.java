fa54a8b544912fbac8440d056c35b24d4481cc20
prashantRR1st <prashxlr8@gmail.com>
2018-01-22 20:05:48 -0500

    Research Management Update

--- bc18-scaffold-master/FalconW/src/ResearchManagement.java
+++ bc18-scaffold-master/FalconW/src/ResearchManagement.java
@@ -12,31 +12,89 @@ public class ResearchManagement {
 	}
 	
 	public void createDefaultQueue(GameController gc){
-
+		gc.queueResearch(UnitType.Worker);
+		gc.queueResearch(UnitType.Worker);
+		gc.queueResearch(UnitType.Rocket);
+		gc.queueResearch(UnitType.Ranger);
+		gc.queueResearch(UnitType.Ranger);
+		gc.queueResearch(UnitType.Worker);
+		gc.queueResearch(UnitType.Worker);
+		gc.queueResearch(UnitType.Knight);
+		gc.queueResearch(UnitType.Mage);
+		gc.queueResearch(UnitType.Knight);
+		gc.queueResearch(UnitType.Mage);
+		gc.queueResearch(UnitType.Knight);
+		gc.queueResearch(UnitType.Mage);
+		gc.queueResearch(UnitType.Rocket);
+		gc.queueResearch(UnitType.Rocket);
+		gc.queueResearch(UnitType.Mage);
+		gc.queueResearch(UnitType.Ranger);
+		gc.queueResearch(UnitType.Healer);
+		gc.queueResearch(UnitType.Healer);
+		gc.queueResearch(UnitType.Healer);
+		
 	}
 
-	public void updateQueueCheck(GameController gc){
-		
+	public void updateQueue(GameController gc, UnitType branch){
+		ResearchInfo ri = new ResearchInfo();
+		VecUnitType currentQueue = ri.queue();
+		if (gc.resetResearch()==1){
+			switch(branch){
+				case Rocket: {
+					raceToMarsProtocol(gc, ri);
+				} case Mage: {
+					blinkRushProtocol(gc, ri);
+				} case Ranger: {
+					snipeRushProtocol(gc, ri);
+				} case Knight: {
+					javelinRushProtocol(gc, ri);
+				} case Worker: {
+					workerBoostProtocol(gc, ri);
+				} case Healer: {
+					healerBoostProtocol(gc, ri);
+				}
+			}
+			for (long i = 0; i<currentQueue.size(); i++){
+				gc.queueResearch(currentQueue.get(i));
+			}
+		}
 	}
 	
-	public void raceToMarsProtocol(GameController gc){
-		
+	public void raceToMarsProtocol(GameController gc, ResearchInfo ri){
+		for (long i=0; i< 3 - ri.getLevel(UnitType.Rocket); i++ ){
+			gc.queueResearch(UnitType.Rocket);	
+		}
 	}
 
-	public void blinkRushProtocol(GameController gc){
-		
+	public void blinkRushProtocol(GameController gc, ResearchInfo ri){
+		for (long i=0; i< 4 - ri.getLevel(UnitType.Mage); i++ ){
+			gc.queueResearch(UnitType.Rocket);	
+		}
+
 	}
 	
-	public void snipeRushProtocol(GameController gc){
-		
+	public void snipeRushProtocol(GameController gc, ResearchInfo ri){
+		for (long i=0; i< 3 - ri.getLevel(UnitType.Ranger); i++ ){
+			gc.queueResearch(UnitType.Rocket);	
+		}
 	}
 	
-	public void javelinRushQueue(GameController gc){
-		
+	public void javelinRushProtocol(GameController gc, ResearchInfo ri){
+		for (long i=0; i< 3 - ri.getLevel(UnitType.Knight); i++ ){
+			gc.queueResearch(UnitType.Rocket);	
+		}
 	}
 
-	public void workerBoostProtocol(GameController gc){
-		
+	public void workerBoostProtocol(GameController gc, ResearchInfo ri){
+		for (long i=0; i< 4 - ri.getLevel(UnitType.Worker); i++ ){
+			gc.queueResearch(UnitType.Rocket);	
+		}
+	}
+
+	public void healerBoostProtocol(GameController gc, ResearchInfo ri){
+		for (long i=0; i< 3 - ri.getLevel(UnitType.Healer); i++ ){
+			gc.queueResearch(UnitType.Rocket);	
+		}
 	}
 	
 }
